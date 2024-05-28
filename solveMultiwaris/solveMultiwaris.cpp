86f5c97acb530e064df4f813e222faadb97f2413
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-28 15:01:57 +0700

    create class budi yang diwarisi dari pekerja dan pelajar

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -33,6 +33,18 @@ public:
 
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+
+};
+
 int main()
 {
     
